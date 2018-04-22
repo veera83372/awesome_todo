c04381cc365d3d2c109563b34e9c9db13795658b
veera83372 <veeravenky222@gmail.com>
2018-04-22 18:03:57 +0530

    Updation is awesome in rails 🔨🛠

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -6,16 +6,36 @@ class TodoListsController < ApplicationController
     @todo_list=TodoList.new
   end
 
+  def edit
+    @todo_list=TodoList.find(params[:id])
+  end
+
   def create
      # render plain: params[:todo_list].inspect
-     @todo_list=TodoList.new(params.require(:todo_list).permit(:title, :description))
+     @todo_list=TodoList.new(todo_list_params)
      if @todo_list.save
        redirect_to @todo_list
      else
        render 'new'
      end
   end
+
+
+  def update
+  @todo_list = TodoList.find(params[:id])
+  if @todo_list.update(todo_list_params)
+    redirect_to @todo_list
+  else
+    render 'edit'
+  end
+  end
+
   def show
     @todo_list=TodoList.find(params[:id])
   end
+  private
+  def todo_list_params
+    params.require(:todo_list).permit(:title, :description)
+  end
+
 end
