b2087060d678884fc69fdb2561efe46feefad387
veera83372 <veeravenky222@gmail.com>
2018-04-22 18:11:27 +0530

    Hey good news you can reduce code duplication using layouts 😎😎

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -33,6 +33,13 @@ class TodoListsController < ApplicationController
   def show
     @todo_list=TodoList.find(params[:id])
   end
+
+  def destroy
+  @todo_list = TodoList.find(params[:id])
+  @todo_list.destroy
+  redirect_to todo_lists_path
+  end
+
   private
   def todo_list_params
     params.require(:todo_list).permit(:title, :description)
